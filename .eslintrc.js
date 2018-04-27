e6e618505d64aff328199b9ede8670849e47d855
shianqi <shianqi@imudges.com>
2018-04-27 23:36:11 +0800

    :lipstick: Add babel & test config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,14 @@
 module.exports = {
-    "extends": "standard"
+  extends: 'standard',
+  rules: {
+    'comma-dangle': [2, 'only-multiline'],
+    'no-unused-vars': 1,
+    'semi': [2, 'always'],
+  },
+  plugins: [
+    'mocha',
+  ],
+  env: {
+    mocha: true,
+  },
 };
